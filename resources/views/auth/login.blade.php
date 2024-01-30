b6b5937ebe4a338d2205ff020d2facdc63b6248b
Afiifatuts Tsaaniyah Abdullah <afiifatuts04@gmail.com>
2024-01-30 11:04:34 +0700

    login and register feature

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,48 +1,72 @@
-<x-guest-layout>
-    <x-authentication-card>
-        <x-slot name="logo">
-            <x-authentication-card-logo />
-        </x-slot>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <head>
+  <meta charset="utf-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <title>Login Page</title>
+  <!-- GOOGLE FONTS -->
+  <link href="https://fonts.googleapis.com/css?family=Karla:400,700|Roboto" rel="stylesheet">
+  <link href="{{ asset('assets/plugins/material/css/materialdesignicons.min.css') }}" rel="stylesheet" />
+  <link href="{{ asset('assets/plugins/simplebar/simplebar.css') }}" rel="stylesheet" />
+  <!-- PLUGINS CSS STYLE -->
+  <link href="{{asset('assets/plugins/nprogress/nprogress.css')}}" rel="stylesheet" />
+  <!-- MONO CSS -->
+  <link id="main-css-href" rel="stylesheet" href="{{ asset('assets/css/style.css') }}" />
+  <!-- FAVICON -->
+  <link href="{{ asset('assets/images/favicon.png') }}" rel="shortcut icon" />
+  <script src="{{ asset('assets/plugins/nprogress/nprogress.js') }}"></script>
+</head>
+</head>
+  <body class="bg-light-gray" id="body">
+          <div class="container d-flex align-items-center justify-content-center" style="min-height: 100vh">
+          <div class="d-flex flex-column justify-content-between">
+            <div class="row justify-content-center">
+              <div class="col-lg-6 col-md-10">
+                <div class="card card-default mb-0">
+                  <div class="card-header pb-0">
+                    <div class="app-brand w-100 d-flex justify-content-center border-bottom-0">
+                      <a class="w-auto pl-0" href="/index.html">
+                        <span class="brand-name text-dark">Welcome !</span>
+                      </a>
+                    </div>
+                  </div>
+                  <div class="card-body px-5 pb-5 pt-0">
 
-        <x-validation-errors class="mb-4" />
+                    <h4 class="text-dark mb-6 text-center">Sign in for free</h4>
+                    <x-validation-errors class="mb-4 text-danger" />
+                    <form method="POST" action="{{ route('login') }}">
+                        @csrf
 
-        @if (session('status'))
-            <div class="mb-4 font-medium text-sm text-green-600">
-                {{ session('status') }}
+                      <div class="row">
+                        <div class="form-group col-md-12 mb-4">
+                            <label for="email">Email</label>
+                            <input  class="form-control input-lg" aria-describedby="emailHelp" id="email" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" placeholder="Email">
+                        </div>
+                        <div class="form-group col-md-12 ">
+                            <label for="email">Password</label>
+                          <input type="password" class="form-control input-lg" id="password" placeholder="Password" name="password" required>
+                        </div>
+                        <div class="col-md-12">
+
+                          <div class="d-flex justify-content-between mb-3">
+                            @if (Route::has('password.request'))
+                            <a class="text-color" href="{{ route('password.request') }}"> Forgot password? </a>
+                            @endif
+                          </div>
+                          <button type="submit" class="btn btn-primary btn-pill mb-4">Sign In</button>
+                          <p>Don't have an account yet ?
+                            <a class="text-blue" href="{{ route('register') }}">Sign Up</a>
+                          </p>
+                        </div>
+                      </div>
+                    </form>
+                  </div>
+                </div>
+              </div>
             </div>
-        @endif
-
-        <form method="POST" action="{{ route('login') }}">
-            @csrf
-
-            <div>
-                <x-label for="email" value="{{ __('Email') }}" />
-                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
-            </div>
-
-            <div class="mt-4">
-                <x-label for="password" value="{{ __('Password') }}" />
-                <x-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="current-password" />
-            </div>
-
-            <div class="block mt-4">
-                <label for="remember_me" class="flex items-center">
-                    <x-checkbox id="remember_me" name="remember" />
-                    <span class="ms-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
-                </label>
-            </div>
-
-            <div class="flex items-center justify-end mt-4">
-                @if (Route::has('password.request'))
-                    <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
-                        {{ __('Forgot your password?') }}
-                    </a>
-                @endif
-
-                <x-button class="ms-4">
-                    {{ __('Log in') }}
-                </x-button>
-            </div>
-        </form>
-    </x-authentication-card>
-</x-guest-layout>
+          </div>
+        </div>
+</body>
+</html>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,60 +1,112 @@
-<x-guest-layout>
-    <x-authentication-card>
-        <x-slot name="logo">
-            <x-authentication-card-logo />
-        </x-slot>
 
-        <x-validation-errors class="mb-4" />
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <head>
+  <meta charset="utf-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
 
-        <form method="POST" action="{{ route('register') }}">
-            @csrf
+  <title>Register</title>
 
-            <div>
-                <x-label for="name" value="{{ __('Name') }}" />
-                <x-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
-            </div>
+  <!-- GOOGLE FONTS -->
+  <link href="https://fonts.googleapis.com/css?family=Karla:400,700|Roboto" rel="stylesheet">
+  <link href="{{ asset('assets/plugins/material/css/materialdesignicons.min.css') }}" rel="stylesheet" />
+  <link href="{{ asset('assets/plugins/simplebar/simplebar.css') }}" rel="stylesheet" />
 
-            <div class="mt-4">
-                <x-label for="email" value="{{ __('Email') }}" />
-                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="username" />
-            </div>
+  <!-- PLUGINS CSS STYLE -->
+  <link href="{{ asset('assets/plugins/nprogress/nprogress.css') }}" rel="stylesheet" />
 
-            <div class="mt-4">
-                <x-label for="password" value="{{ __('Password') }}" />
-                <x-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="new-password" />
-            </div>
+  <!-- MONO CSS -->
+  <link id="main-css-href" rel="stylesheet" href="{{ asset('assets/css/style.css') }}" />
 
-            <div class="mt-4">
-                <x-label for="password_confirmation" value="{{ __('Confirm Password') }}" />
-                <x-input id="password_confirmation" class="block mt-1 w-full" type="password" name="password_confirmation" required autocomplete="new-password" />
-            </div>
 
-            @if (Laravel\Jetstream\Jetstream::hasTermsAndPrivacyPolicyFeature())
-                <div class="mt-4">
-                    <x-label for="terms">
-                        <div class="flex items-center">
-                            <x-checkbox name="terms" id="terms" required />
 
-                            <div class="ms-2">
-                                {!! __('I agree to the :terms_of_service and :privacy_policy', [
-                                        'terms_of_service' => '<a target="_blank" href="'.route('terms.show').'" class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">'.__('Terms of Service').'</a>',
-                                        'privacy_policy' => '<a target="_blank" href="'.route('policy.show').'" class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">'.__('Privacy Policy').'</a>',
-                                ]) !!}
-                            </div>
+
+  <!-- FAVICON -->
+  <link href="{{ asset('assets/images/favicon.png') }}" rel="shortcut icon" />
+  <script src="{{ asset('assets/plugins/nprogress/nprogress.js') }}"></script>
+</head>
+
+</head>
+  <body class="bg-light-gray" id="body">
+          <div class="container d-flex align-items-center justify-content-center" style="min-height: 100vh">
+          <div class="d-flex flex-column justify-content-between">
+            <div class="row justify-content-center">
+              <div class="col-lg-6 col-xl-5 col-md-10 ">
+                <div class="card card-default mb-0">
+                  <div class="card-header pb-0">
+                    <div class="app-brand w-100 d-flex justify-content-center border-bottom-0">
+                      <a class="w-auto pl-0" href="/">
+                        <span class="brand-name text-dark">Welcome!!</span>
+                      </a>
+                    </div>
+                  </div>
+                  <div class="card-body px-5 pb-5 pt-0">
+                    <h4 class="text-dark text-center mb-5">Sign Up</h4>
+                    <x-validation-errors class="mb-4 text-danger" />
+
+                    <form method="POST" action="{{ route('register') }}">
+                        @csrf
+                      <div class="row">
+                        <div class="form-group col-md-12 mb-4">
+                            <label for="name">Name</label>
+                          <input type="text" class="form-control input-lg" id="name" name="name" aria-describedby="nameHelp" :value="old('name')" placeholder="Name" autofocus required>
                         </div>
-                    </x-label>
+                        <div class="form-group col-md-12 mb-4">
+                            <label for="email">Email</label>
+                          <input type="email" name="email" class="form-control input-lg" id="email" aria-describedby="emailHelp" required placeholder="Email">
+                        </div>
+                        <div class="form-group col-md-12 ">
+                            <label for="password">Password</label>
+                          <input type="password" name="password" class="form-control input-lg" id="password" placeholder="New Password">
+                        </div>
+                        <div class="form-group col-md-12 ">
+                            <label for="password_confirmation">Confirm Password</label>
+                          <input type="password" class="form-control input-lg" id="password_confirmation" name="password_confirmation" placeholder="Confirm Password">
+                        </div>
+                        <div class="col-md-12">
+                            @if (Laravel\Jetstream\Jetstream::hasTermsAndPrivacyPolicyFeature())
+
+                          <div class="d-flex justify-content-between mb-3">
+
+
+                            <div class="custom-control custom-checkbox mr-3 mb-3">
+                              {{-- <input type="checkbox" class="custom-control-input" name="terms" id="terms" required > --}}
+                              <x-checkbox name="terms" id="terms" required />
+
+                              {!! __('I agree to the :terms_of_service and :privacy_policy', [
+                                'terms_of_service' => '<a target="_blank" href="'.route('terms.show').'" class="underline text-sm">Terms of Service</a>',
+                                'privacy_policy' => '<a target="_blank" href="'.route('policy.show').'" class="underline text-sm text-gray-600">Privacy Policy</a>',
+                        ]) !!}
+
+                              {{-- <label class="custom-control-label" for="customCheck2">I Agree the
+
+
+                                <a target="_blank" href="'.route('terms.show').'">Terms of Service</a>
+                                 and
+
+                                 <a target="_blank" href="'.route('policy.show').'">
+                                    Privacy Policy</a>.</label> --}}
+                            </div>
+
+                          </div>
+                          @endif
+
+                          <button type="submit" class="btn btn-primary btn-pill mb-4">Sign Up</button>
+
+                          <p>Already have an account?
+                            <a class="text-blue" href="{{ route('login') }}">Sign in</a>
+                          </p>
+                        </div>
+                      </div>
+                    </form>
+
+                  </div>
                 </div>
-            @endif
-
-            <div class="flex items-center justify-end mt-4">
-                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('login') }}">
-                    {{ __('Already registered?') }}
-                </a>
-
-                <x-button class="ms-4">
-                    {{ __('Register') }}
-                </x-button>
+              </div>
             </div>
-        </form>
-    </x-authentication-card>
-</x-guest-layout>
+          </div>
+        </div>
+</body>
+</html>
